25a924c6e7d97fdea7b1709d4b11c1639030d8d4
I-AM-VLAD <vlad1slaw19982@gmail.com>
2022-01-06 00:40:44 +0200

    Liquibase. Интеграционные тесты. [#95881]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -11,11 +11,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
@@ -52,11 +52,63 @@ public class SqlTrackerTest {
     }
 
     @Test
-    public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+    public void testAdd() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        assertThat(tracker.add(item), is(item));
+    }
+
+    @Test
+    public void testFindById() {
         SqlTracker tracker = new SqlTracker(connection);
         Item item = new Item("item");
         tracker.add(item);
         assertThat(tracker.findById(item.getId()), is(item));
+    }
+
+    @Test
+    public void testFindByName() {
+        SqlTracker tracker = new SqlTracker(connection);
+        List<Item> expected = new ArrayList<>();
+        Item item1 = new Item("item1");
+        Item item2 = new Item("item2");
+        Item item3 = new Item("item3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        expected.add(item1);
+        assertThat(tracker.findByName("item1"), is(expected));
+    }
+
+    @Test
+    public void testFindAll() {
+        SqlTracker tracker = new SqlTracker(connection);
+        List<Item> expected = new ArrayList<>();
+        Item item1 = new Item("item1");
+        Item item2 = new Item("item2");
+        Item item3 = new Item("item3");
+        tracker.add(item1);
+        tracker.add(item2);
+        tracker.add(item3);
+        expected.add(item1);
+        expected.add(item2);
+        expected.add(item3);
+        assertThat(tracker.findAll(), is(expected));
+    }
+    @Test
+    public void testDelete() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        tracker.add(item);
+        assertThat(tracker.delete(item.getId()), is(true));
+    }
+    @Test
+    public void testReplace() {
+        SqlTracker tracker = new SqlTracker(connection);
+        Item item = new Item("item");
+        Item newItem = new Item("newItem");
+        tracker.add(item);
+        assertThat(tracker.replace(item.getId(), newItem), is(true));
     }
 
 }
